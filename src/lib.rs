47f53620a9b49066798e8988848a4844ed073787
Karl Wulfert <pic16f877.ccs@emailn.de>
2023-03-03 22:37:52 +0100

    Rename to TryTupToArr

--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,13 @@ use proc_macro::TokenStream;
 fn generic_type_param(n: usize) -> String {
     (0..=n).map(|i| format!("T{i}, ")).collect::<String>()
 }                                                                                                                                                            
-fn from_tup_fn_ident(n: usize) -> String {
+fn tup_to_fn_ident(n: usize) -> String {
     (0..=n) 
         .map(|i| format!(
 "            match U::try_from_int_str(self.{i}) {{
                     Ok(ok) => ok,
                     Err(err) => {{
-                        return Err(TryFromTupErr {{
+                        return Err(TryTupToArrErr {{
                             source: err,
                             posice: {pos},
                         }})
@@ -24,17 +24,17 @@ fn from_tup_fn_ident(n: usize) -> String {
         .collect::<String>()
 }           
             
-fn from_tup_type_bound(n: usize) -> String {
+fn tup_to_type_bound(n: usize) -> String {
     (0..=n) 
         .map(|i| format!("TryFromIntStr<T{i}> + "))
         .collect::<String>()
 }
 
 #[rustfmt::skip]
-fn from_tup_impl_code(n: usize) -> String {
+fn tup_to_impl_code(n: usize) -> String {
     (0..=n).map(|i| format!(
 "    
-impl<U, {type_param}> TupToArr<U> for ({type_param})
+impl<U, {type_param}> TryTupToArr<U> for ({type_param})
 where
     U: {type_bound},
 {{   
@@ -42,7 +42,7 @@ where
      
     #[doc = \"Converts tuple ({type_doc}) to array [Self; {i}].\"]
     #[inline] 
-    fn into_arr(self) -> Result<Self::A, TryFromTupErr> {{
+    fn try_into_arr(self) -> Result<Self::A, TryTupToArrErr> {{
         Ok([
 {fn_ident}        
         ])  
@@ -50,127 +50,127 @@ where
 }}
 ",
         type_param = generic_type_param(i),
-        fn_ident = from_tup_fn_ident(i),
-        type_bound = from_tup_type_bound(i),
+        fn_ident = tup_to_fn_ident(i),
+        type_bound = tup_to_type_bound(i),
         type_doc = generic_type_param(i).trim_end(),
         i = i + 1,)).collect::<String>()
 }
 
-macro_rules! try_from_tup_trait {
+macro_rules! try_tup_to_arr_trait {
     ($to:expr) => {
 
         #[proc_macro]
-        pub fn tup_from_impl(_item: TokenStream) -> TokenStream {
-            from_tup_impl_code($to - 1).parse().unwrap()
+        pub fn try_tup_to_arr_impl(_item: TokenStream) -> TokenStream {
+            tup_to_impl_code($to - 1).parse().unwrap()
         }
     };
 }
 
 #[cfg(all(
-    feature = "try_from_tup_8",
-    not(feature = "try_from_tup_16"),
-    not(feature = "try_from_tup_32"),
-    not(feature = "try_from_tup_64")
+    feature = "try_tup_to_arr_8",
+    not(feature = "try_tup_to_arr_16"),
+    not(feature = "try_tup_to_arr_32"),
+    not(feature = "try_tup_to_arr_64")
 ))]
-try_from_tup_trait!(8);
+try_tup_to_arr_trait!(8);
 
 #[cfg(all(
-    feature = "try_from_tup_16",
-    not(feature = "try_from_tup_8"),
-    not(feature = "try_from_tup_32"),
-    not(feature = "try_from_tup_64")
+    feature = "try_tup_to_arr_16",
+    not(feature = "try_tup_to_arr_8"),
+    not(feature = "try_tup_to_arr_32"),
+    not(feature = "try_tup_to_arr_64")
 ))]
-try_from_tup_trait!(16);
+try_tup_to_arr_trait!(16);
 #[cfg(all(
-    feature = "try_from_tup_16",
-    feature = "try_from_tup_8",
-    not(feature = "try_from_tup_32"),
-    not(feature = "try_from_tup_64")
+    feature = "try_tup_to_arr_16",
+    feature = "try_tup_to_arr_8",
+    not(feature = "try_tup_to_arr_32"),
+    not(feature = "try_tup_to_arr_64")
 ))]
-try_from_tup_trait!(16);
+try_tup_to_arr_trait!(16);
 
 #[cfg(all(
-    feature = "try_from_tup_32",
-    not(feature = "try_from_tup_8"),
-    not(feature = "try_from_tup_16"),
-    not(feature = "try_from_tup_64")
+    feature = "try_tup_to_arr_32",
+    not(feature = "try_tup_to_arr_8"),
+    not(feature = "try_tup_to_arr_16"),
+    not(feature = "try_tup_to_arr_64")
 ))]
-try_from_tup_trait!(32);
+try_tup_to_arr_trait!(32);
 #[cfg(all(
-    feature = "try_from_tup_32",
-    feature = "try_from_tup_8",
-    not(feature = "try_from_tup_16"),
-    not(feature = "try_from_tup_64")
+    feature = "try_tup_to_arr_32",
+    feature = "try_tup_to_arr_8",
+    not(feature = "try_tup_to_arr_16"),
+    not(feature = "try_tup_to_arr_64")
 ))]
-try_from_tup_trait!(32);
+try_tup_to_arr_trait!(32);
 #[cfg(all(
-    feature = "try_from_tup_32",
-    feature = "try_from_tup_16",
-    not(feature = "try_from_tup_8"),
-    not(feature = "try_from_tup_64")
+    feature = "try_tup_to_arr_32",
+    feature = "try_tup_to_arr_16",
+    not(feature = "try_tup_to_arr_8"),
+    not(feature = "try_tup_to_arr_64")
 ))]
-try_from_tup_trait!(32);
+try_tup_to_arr_trait!(32);
 #[cfg(all(
-    feature = "try_from_tup_32",
-    feature = "try_from_tup_8",
-    feature = "try_from_tup_16",
-    not(feature = "try_from_tup_64")
+    feature = "try_tup_to_arr_32",
+    feature = "try_tup_to_arr_8",
+    feature = "try_tup_to_arr_16",
+    not(feature = "try_tup_to_arr_64")
 ))]
-try_from_tup_trait!(32);
+try_tup_to_arr_trait!(32);
 
 #[cfg(all(
-    feature = "try_from_tup_64",
-    not(feature = "try_from_tup_8"),
-    not(feature = "try_from_tup_16"),
-    not(feature = "try_from_tup_32")
+    feature = "try_tup_to_arr_64",
+    not(feature = "try_tup_to_arr_8"),
+    not(feature = "try_tup_to_arr_16"),
+    not(feature = "try_tup_to_arr_32")
 ))]
-try_from_tup_trait!(64);
+try_tup_to_arr_trait!(64);
 #[cfg(all(
-    feature = "try_from_tup_64",
-    feature = "try_from_tup_8",
-    not(feature = "try_from_tup_16"),
-    not(feature = "try_from_tup_32")
+    feature = "try_tup_to_arr_64",
+    feature = "try_tup_to_arr_8",
+    not(feature = "try_tup_to_arr_16"),
+    not(feature = "try_tup_to_arr_32")
 ))]
-try_from_tup_trait!(64);
+try_tup_to_arr_trait!(64);
 #[cfg(all(
-    feature = "try_from_tup_64",
-    feature = "try_from_tup_16",
-    not(feature = "try_from_tup_8"),
-    not(feature = "try_from_tup_32")
+    feature = "try_tup_to_arr_64",
+    feature = "try_tup_to_arr_16",
+    not(feature = "try_tup_to_arr_8"),
+    not(feature = "try_tup_to_arr_32")
 ))]
-try_from_tup_trait!(64);
+try_tup_to_arr_trait!(64);
 #[cfg(all(
-    feature = "try_from_tup_64",
-    feature = "try_from_tup_32",
-    not(feature = "try_from_tup_8"),
-    not(feature = "try_from_tup_16")
+    feature = "try_tup_to_arr_64",
+    feature = "try_tup_to_arr_32",
+    not(feature = "try_tup_to_arr_8"),
+    not(feature = "try_tup_to_arr_16")
 ))]
-try_from_tup_trait!(64);
+try_tup_to_arr_trait!(64);
 #[cfg(all(
-    feature = "try_from_tup_64",
-    feature = "try_from_tup_8",
-    feature = "try_from_tup_16",
-    not(feature = "try_from_tup_32")
+    feature = "try_tup_to_arr_64",
+    feature = "try_tup_to_arr_8",
+    feature = "try_tup_to_arr_16",
+    not(feature = "try_tup_to_arr_32")
 ))]
-try_from_tup_trait!(64);
+try_tup_to_arr_trait!(64);
 #[cfg(all(
-    feature = "try_from_tup_64",
-    feature = "try_from_tup_8",
-    feature = "try_from_tup_32",
-    not(feature = "try_from_tup_16")
+    feature = "try_tup_to_arr_64",
+    feature = "try_tup_to_arr_8",
+    feature = "try_tup_to_arr_32",
+    not(feature = "try_tup_to_arr_16")
 ))]
-try_from_tup_trait!(64);
+try_tup_to_arr_trait!(64);
 #[cfg(all(
-    feature = "try_from_tup_64",
-    feature = "try_from_tup_16",
-    feature = "try_from_tup_32",
-    not(feature = "try_from_tup_8")
+    feature = "try_tup_to_arr_64",
+    feature = "try_tup_to_arr_16",
+    feature = "try_tup_to_arr_32",
+    not(feature = "try_tup_to_arr_8")
 ))]
-try_from_tup_trait!(64);
+try_tup_to_arr_trait!(64);
 #[cfg(all(
-    feature = "try_from_tup_64",
-    feature = "try_from_tup_8",
-    feature = "try_from_tup_16",
-    feature = "try_from_tup_32"
+    feature = "try_tup_to_arr_64",
+    feature = "try_tup_to_arr_8",
+    feature = "try_tup_to_arr_16",
+    feature = "try_tup_to_arr_32"
 ))]
-try_from_tup_trait!(64);
+try_tup_to_arr_trait!(64);
